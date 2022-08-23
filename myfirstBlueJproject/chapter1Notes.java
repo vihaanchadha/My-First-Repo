0af2ca9f014c521d1e388e8906b316d37bf0f690
vihaanchadha <vchadha@stu.naperville203.org>
2022-08-23 12:39:26 -0500

    Started bluej and chapter 1 notes

---
+++ myfirstBlueJproject/chapter1Notes.java
@@ -0,0 +1,44 @@
+
+/**
+ * Vihaan Chadha 
+ * 8/23/22
+ * Notes for Chapter 1
+ * Write a description where we are going to take some code notes 
+ * Who helped me: Mr. Hayes, Mom, Dog
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class chapter1Notes
+{
+    /**
+     * main method 
+     * where we are going to code 
+     */
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        //these are the basic data types 
+        int x=9,y,z;
+        double num=5, num1=23.4567, num2;
+        
+        //A char is one singular thing 
+        char letter= 'd';
+        boolean choice = false; 
+        float anum = 7; 
+        long a=9; 
+        short b=5; 
+        
+        
+        
+        //These are objects!
+        //A string is a series of chars 
+        String word = new String("Vihaan Chadha");
+        Integer number  = new Integer(47);
+        Double number2  = new Double(34.5);
+        
+     
+    }
+}
